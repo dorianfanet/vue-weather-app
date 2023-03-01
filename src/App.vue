f36cfb7c46203be37df2d14869a6bff21004ed50
dorianfanet <dorian.fanet@gmail.com>
2023-03-01 18:42:21 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Header from './components/Header.vue';
+
+const childMsg = ref('')
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <h1>Paris, France</h1>
+    <input type="text">
+  </header>
+</template>
+
+<style>
+.header{
+  display: flex;
+}
+</style>
