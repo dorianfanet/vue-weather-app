7337e72a95c92a7da3b42ee2db97f7c2925271e3
dorianfanet <dorian.fanet@gmail.com>
2023-03-01 18:50:29 +0100

    header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,34 @@
 <template>
   <header class="header">
     <h1>Paris, France</h1>
-    <input type="text">
+    <input type="text" placeholder="Search location">
   </header>
 </template>
 
 <style>
 .header{
   display: flex;
+  position: relative;
+  align-items: center;
 }
+
+.header h1{
+  color: white;
+  margin: 0;
+  font-size: 28px;
+}
+
+.header input{
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 40px;
+  width: 40%;
+  background-color: var(--secondaryColor);
+  border: 0;
+  border-radius: 20px;
+  font-family: 'Outfit';
+  padding: 0 0 0 2em;
+}
+
 </style>
